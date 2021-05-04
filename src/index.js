8085fb4b4809f3861181992c393d5cb0a6abe6bf
DevinBayly <baylyd@email.arizona.edu>
2021-05-03 17:31:31 -0700

    looks like plane has physics

---
+++ src/index.js
@@ -0,0 +1,66 @@
+AFRAME.registerComponent('player',{
+  init() {
+    console.log("creating player")
+    let el = this.el
+    let scene = el.sceneEl
+    window.addEventListener("keydown",(e)=> {
+      let sign = 1
+      if (e.shiftKey) {
+        sign=-1
+      }
+      if (e.code.match(/space/i)) {
+        el.object3D.position.y += .1*sign
+      }
+    })
+    let ele = this.el
+    window.addEventListener("shrine-placed",(e)=> {
+      let shrine_pos =  e.detail.srcElement.object3D.children[0].children[0].position
+      ele.object3D.position.x = shrine_pos.x
+      ele.object3D.position.y = shrine_pos.y+.5
+      ele.object3D.position.z = shrine_pos.z
+      console.log(ele.object3D.position,"position now")
+    })
+  }
+})
+
+// raycasting collisin check, add this to something and it will alert when the raycaster hits something
+//
+AFRAME.registerComponent('collider-check', {
+  dependencies:['raycaster'],
+  init: function () {
+    this.el.addEventListener('raycaster-intersection',function(e) {
+      console.log('player hit something',e)
+    })
+  }
+})
+
+// this function will make it so that our camera rig gets an event to place at the right location
+AFRAME.registerComponent("loader-listener",{
+  init: function () {
+    this.el.addEventListener('model-loaded',function(e){ 
+      console.log("loaded model",e)
+      let event = new CustomEvent('shrine-placed',{detail:e})
+      window.dispatchEvent(event)
+    })
+  }
+})
+
+AFRAME.registerComponent("crosses",{
+  init:function() {
+    // load the elementsjson, then append a box for each 
+    let sEl = this.el.sceneEl
+    fetch("../assets/processed_files/elements.json").then(res=> res.json()).then(j=> {
+      for(let cross of j){
+        //let b = document.createElement("a-gltf-model")
+        let b = document.createElement("a-box")
+        //b.setAttribute("src","../assets/processed_files/cross.glb")
+      b.setAttribute("position",`${cross.pt.x} ${cross.pt.y} ${cross.pt.z}`)
+      b.setAttribute("color","red")
+      b.setAttribute("scale",".1 .1 .1")
+      sEl.append(b)
+      }
+      
+    })
+  }
+})
+
