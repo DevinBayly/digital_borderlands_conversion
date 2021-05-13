067bde1947b568f19c2ec9212162da735e5d99d0
DevinBayly <baylyd@email.arizona.edu>
2021-05-13 16:43:22 -0700

    create bidirectional travel to shrine from portal

--- src/mesh_test.js
+++ src/mesh_test.js
@@ -1,6 +1,5 @@
 function makeBox(p) {
   let box = document.createElement("a-box")
-  box.setAttribute("color","red")
   box.setAttribute("scale","1 1 1")
   box.object3D.position.x = p.x
   box.object3D.position.y = p.y
@@ -83,6 +82,25 @@ AFRAME.registerComponent("raycasttest",{
   }
 })
 
+function raycastPortalPlacement() {
+      // fetch the landscape
+      let landscape = document.querySelector("#ray-landscape").object3D
+      let portal = document.querySelector("#portal")
+      console.log("landscape is" ,landscape)
+      // raycast a location near the user
+      let start = new THREE.Vector3(0,-2000,0)
+      let dir = new THREE.Vector3(0,1,0)
+      let ray = new THREE.Raycaster(start,dir)
+      let intersect = ray.intersectObject(landscape.children[0],true)
+      console.log("intersect is ",intersect)
+      let p = intersect[0].point
+      // use the intersect point as a position
+      portal.setAttribute("position",`${p.x} ${p.y} ${p.z}`)
+    // place it on the ground somewhere
+
+      // 
+}
+
 AFRAME.registerComponent('cross-loader', {
   async init() {
     let loader = new THREE.GLTFLoader()
@@ -91,6 +109,9 @@ AFRAME.registerComponent('cross-loader', {
 
       // use the bb on the landscape to correctly scale the 0-1 values from the death data json 
       let scene = gltf.scene
+      // set object for other systems to load off of
+      console.log("starting crossese")
+      el.setObject3D('rayland',gltf.scene)
       let geometry = gltf.scene.children[0].geometry
       let data = await fetch("../assets/processed_files/death_points_n33_w113.json").then(res=> res.json())
       let bb = geometry.boundingBox
@@ -113,8 +134,8 @@ AFRAME.registerComponent('cross-loader', {
         let cross = crossesInMeshSpace[i]
         if (i == 0) { 
           // move to the first cross
-          cam.object3D.position.x = cross.position.x
-          cam.object3D.position.z = cross.position.z
+          //cam.object3D.position.x = cross.position.x
+          //cam.object3D.position.z = cross.position.z
         }
         let dst = cam.object3D.position.distanceTo(cross.position) 
         if (dst < 8000) {
@@ -139,6 +160,8 @@ AFRAME.registerComponent('cross-loader', {
       sequentialLoads.map(
        e=> e.then(i=> crossExecute(i))
       )
+      // do the portal raycast placement
+      //raycastPortalPlacement()
     })
   }
 })
@@ -149,20 +172,65 @@ AFRAME.registerComponent('collider-check', {
   init: function () {
     let avatar = document.querySelector("#avatar")
     let cam = document.querySelector("#camera")
+    let landscapeEl = document.querySelector("#actual-landscape")
+    let shrineEl = document.querySelector("#shrineEL")
     this.el.addEventListener('click', function (e) {
       let point =  e.detail.intersection.point
-      if (e.detail.intersectedEl.getAttribute("gltf-model").match(/cross/) == null) {
+      let intersectedEl = e.detail.intersectedEl
+      if (intersectedEl === landscapeEl || intersectedEl === shrineEl ) {
       console.log('Player hit something!',e.detail);
-      // make a little box at location
+
+      // make a little box at location if on landscapee
+      if (intersectedEl === landscapeEl ) {
       makeBox(point)
+      avatar.object3D.position.y = point.y + 2
+      } else {
+        // change height adjust for indoor
+      avatar.object3D.position.y = point.y +.05
+      }
       cam.object3D.position.x = point.x
-      avatar.object3D.position.y = point.y +.5
       cam.object3D.position.z = point.z
       }
       //
     });
   }
 });
+
+AFRAME.registerComponent('my-gltf-model',{
+  init() { 
+    let el = this.el
+    let loader = new THREE.GLTFLoader()
+  loader.load('../assets/processed_files/test_collision_box6_visible.glb',function (gltf) {
+    // goal of doing things this way is to modify the uv's in the geometry so that we eliminate the patterns that appear over the landscape
+    let geo = gltf.scene.children[0].geometry
+    let inds = geo.index.array
+    let uvs = geo.attributes.uv.array
+    // go through inds getting groups of three
+    for (let i = 0 ; i < inds.length;i+=3) {
+      let faceInds = inds.slice(i,i+3)
+      // this will be the amount we add to each of the uv thetas so they remain relative to each other except rotated
+      let thetaBump = Math.random()*2*Math.PI
+      for (let fi of faceInds) {
+        // use the fi to get the u,v from the uvs
+        let u = uvs[fi]
+        let v = uvs[fi+1]
+        let mag = Math.sqrt(u*u+v*v)
+        let theta = Math.atan(v/u)
+        let newTheta =theta+ thetaBump
+        let newU = Math.cos(newTheta)*mag
+        let newV = Math.sin(newTheta)*mag
+        uvs[fi] = newU
+        uvs[fi+1] = newV
+      }
+    }
+    // set the object as our entity
+    el.setObject3D('landscape',gltf.scene)
+
+
+  })
+
+  }
+})
 
 // Component to change to a sequential color on click.
 AFRAME.registerComponent('cursor-listener', {
@@ -172,3 +240,35 @@ AFRAME.registerComponent('cursor-listener', {
     });
   }
 });
+
+AFRAME.registerComponent('stairs-loader',{
+  init () {
+    // 
+    this.el.addEventListener('click',function(e) {
+      // transport to the portal
+      console.log("clicked stairs",e)
+      let portal = document.querySelector("#portalEL").object3D.children[0].children[0].position
+      let cam = document.querySelector("#camera").object3D.position
+      let avatar = document.querySelector("#avatar").object3D.position
+      cam.x = portal.x-2
+      cam.z = portal.z + 1
+      avatar.y = portal.y+2
+    })
+  }
+})
+AFRAME.registerComponent('portal-loader',{
+  init () {
+    // 
+    this.el.addEventListener('click',function(e) {
+      // transport to the shrine
+      console.log("clicked portal",e)
+      let shrine = document.querySelector("#shrineEL").object3D.children[0].children[0].position
+      let cam = document.querySelector("#camera").object3D.position
+      let avatar = document.querySelector("#avatar").object3D.position
+      cam.x = shrine.x
+      cam.y = 0
+      cam.z = shrine.z
+      avatar.y = shrine.y
+    })
+  }
+})
